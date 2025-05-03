ccc1d715ca196799c38cb21419ac8d26e1d7f742
ankitranjan1028 <aankitrn17@gmail.com>
2025-05-03 23:15:42 +0530

    minor errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://quick-chat-drab.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
