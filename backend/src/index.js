8231e1a02c59cccfafce14cdc6f1487bbfe220b9
ankitranjan1028 <aankitrn17@gmail.com>
2025-05-03 19:58:21 +0530

    origin update

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://quick-chat-drab.vercel.app",
     credentials: true,
   })
 );
